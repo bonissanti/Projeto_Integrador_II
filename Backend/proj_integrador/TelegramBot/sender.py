9bd7cfa947e55ae1bf45af21784cfa4fda2a03c3
Bruno Rodrigues Onissanti <brunoonissanti@gmail.com>
2026-09-06 15:59:54 -0300

    feat(sender): fix telegram sender

--- Backend/proj_integrador/TelegramBot/sender.py
+++ Backend/proj_integrador/TelegramBot/sender.py
@@ -1,5 +1,8 @@
 from botCore.send_message import enviar_mensagem_via_telegram
 
 class TelegramSender:
+    def __init__(self, bot_telefone: str):
+        self.bot_telefone = bot_telefone
+
     def enviar(self, destinatario: str, mensagem: str) -> None:
         enviar_mensagem_via_telegram(destinatario, mensagem)
